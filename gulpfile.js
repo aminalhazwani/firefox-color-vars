15a20d14f5e5f59fc6dc0f770c7406bac1a775b6
Amin Al Hazwani <amin.alhazwani@gmail.com>
2017-06-19 17:22:06 +0200

    [add] json2clr

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var jsonSass = require('gulp-json-sass'),
     gulp = require('gulp'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
-    sequence = require('gulp-sequence');
+    sequence = require('gulp-sequence'),
+    exec = require('gulp-exec'),
+    del = require('del');
 
 gulp.task('json2sass', function() {
   return gulp
@@ -27,4 +29,31 @@ function cssVars(content) {
   return ':root {\n' + transform + '\n}';
 }
 
-gulp.task('colors', sequence('json2sass', 'json2css'));
+gulp.task('json2clr', function(cb) {
+  return gulp
+    .src('firefox-colors.json')
+    .pipe(change(toCapitalize))
+    .pipe(rename('firefox-colors-precompile.json'))
+    .pipe(gulp.dest('./'))
+    .pipe(exec('./json2clr -n "Firefox Colors" -i firefox-colors-precompile.json -o "Firefox Colors.clr"'));;
+})
+
+gulp.task('clean', function(cb){
+  del(['./firefox-colors-precompile.json'], cb);
+});
+
+function toCapitalize(content) {
+  let data = JSON.parse(content);
+  let output = [];
+  for (let color of Object.keys(data)) {
+    let colorObject = data[color];
+    for (let shade of Object.keys(colorObject)) {
+      let colorName = color[0].toUpperCase() + color.slice(1).toLowerCase();
+      let colorHex = colorObject[shade].slice(1, 7)
+      output.push({name: `${colorName} ${shade}`, hex: `${ colorHex }`});
+    }
+  }
+  return JSON.stringify(output);
+}
+
+gulp.task('colors', sequence('json2sass', 'json2css', 'json2clr', 'clean'));
